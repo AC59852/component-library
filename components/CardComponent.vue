55452aad467d514366ec62a3ce2c9fb27fc29866
Austin Caron <austin.caron1@gmail.com>
2024-06-18 18:05:13 -0400

    init commit

---
+++ components/CardComponent.vue
@@ -0,0 +1,58 @@
+<template>
+  <article class="card">
+    <div class="card__icon card__icon--top">
+      <img src="/" alt="icon">
+    </div>
+    <div class="card__text">
+      <h3 class="card__subtitle">10 June 2020</h3>
+      <h2 class="card__title">{{ card.title.name }}</h2>
+      <p class="card__description">The routines are designed to not only give you a full-body workout but also strengthen weak points—and pump up your trophy muscles.</p>
+      <button class="card__btn">
+        <img src="/" alt="icon" class="card__icon card__icon--btn">
+        <span class="card__subtitle card__subtitle--card">Start</span>
+      </button>
+    </div>
+    <div class="card__footer">
+      <div class="card__thumb">
+        <img src="/" alt="icon" class="card__icon card__icon--footer">
+        <span class="card__subtitle card__subtitle--footer">Calories</span>
+      </div>
+      <div class="card__thumb">
+        <img src="/" alt="icon" class="card__icon card__icon--footer">
+        <span class="card__subtitle card__subtitle--footer">45 Min</span>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ['cardData'],
+
+  data() {
+    return {
+      card: this.cardData
+    }
+  },
+
+  // when the card.background changes, update the background image
+  watch: {
+    'card.background': function() {
+      this.$el.style.backgroundImage = `url(${this.card.background})`
+    }
+  }
+}
+</script>
+
+<style>
+  .card__title {
+    color: v-bind(card.title.color)
+  }
+
+  .card {
+    height: 500px;
+    width: 350px;
+    background-size: cover;
+    background-position: center;
+  }
+</style>
---
+++ pages/cards.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <CardComponent :cardData="cardData" />
+    <select name="colorSelect" id="dropdown" v-model="cardData.title.color">
+      <option v-for="color in colors" :value="color">{{ color }}</option>
+    </select>
+    <input type="file" accept="image/*" @change="loadFile($event)">
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        colors: ['red', 'blue', 'green', 'yellow'],
+
+        cardData: {
+          background: '',
+          title: {
+            name: 'Running in the Woods',
+            color: 'green'
+          },
+          subtitle: '10 June 2020',
+          description: 'The routines are designed to not only give you a full-body workout but also strengthen weak points—and pump up your trophy muscles.',
+          btnText: 'Start',
+          btnIcon: '/',
+          footer: [
+            {
+              icon: '/',
+              subtitle: 'Calories'
+            },
+            {
+              icon: '/',
+              subtitle: '45 Min'
+            }
+          ]
+        }
+      }
+    },
+
+    methods: {
+      loadFile(e) {
+        // const reader = new FileReader();
+        // reader.onload = e => variable = e.target.result;
+        // reader.readAsDataURL(this.files[0]);
+
+        // this.cardData.background = variable;
+
+        this.cardData.background = URL.createObjectURL(e.target.files[0]);
+
+        // remove the background image when the file input is cleared
+        if (!e.target.files[0]) {
+          this.cardData.background = '';
+        }
+      }
+    }
+  }
+</script>
